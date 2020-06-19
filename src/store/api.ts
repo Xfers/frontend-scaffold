6931823c127279833d03afb3b8bd7bdcc4131d76
Rebekah Low <rebekahlow.jy@gmail.com>
2020-06-19 13:38:44 +0800

    Add API support

---
+++ src/store/api.ts
@@ -0,0 +1,58 @@
+import axios from 'axios'
+
+const DOMAIN = 'https://www.dnd5eapi.co/api'
+
+export enum ENDPOINTS {
+  SPELL_ACID_ARROW_ENDPOINT = '/spells/acid-arrow',
+  MONSTER_ADULT_BLACK_DRAGON_ENDPOINT = '/monsters/adult-black-dragon/',
+}
+
+async function makeRequest(
+  url: string,
+  method: 'get' | 'post' | 'delete' | 'put',
+  additionalHeaders: Record<string, unknown> = {},
+  requestBody: Record<string, unknown> = {}
+) {
+  const response = await axios({
+    method,
+    url: DOMAIN + url,
+    headers: {
+      ...additionalHeaders,
+    },
+    data: requestBody,
+    validateStatus: (status) => status >= 200 && status < 400, // NOTE: Adding this since axios is handling 304 as error responses, ref: https://github.com/axios/axios/issues/703
+  })
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+  return response.data
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type ResponsePromise = Promise<any>
+
+export function get(endpoint: ENDPOINTS, subRoute: string = ''): ResponsePromise {
+  return makeRequest(endpoint + subRoute, 'get')
+}
+
+export function post(
+  endpoint: ENDPOINTS,
+  requestBody: Record<string, unknown>,
+  additionalHeaders: Record<string, unknown> = {},
+  subRoute: string = ''
+): ResponsePromise {
+  return makeRequest(endpoint + subRoute, 'post', additionalHeaders, requestBody)
+}
+export function del(
+  endpoint: ENDPOINTS,
+  additionalHeaders: Record<string, unknown> = {},
+  subRoute: string = ''
+): ResponsePromise {
+  return makeRequest(endpoint + subRoute, 'delete', additionalHeaders)
+}
+export function put(
+  endpoint: ENDPOINTS,
+  requestBody: Record<string, unknown>,
+  additionalHeaders: Record<string, unknown> = {},
+  subRoute: string = ''
+): ResponsePromise {
+  return makeRequest(endpoint + subRoute, 'put', additionalHeaders, requestBody)
+}
--- src/store/user/actions.ts
+++ src/store/user/actions.ts
@@ -1,9 +1,18 @@
 import { Dispatch } from '~/store/types'
 import { ActionTypes, USER_ACTIONS } from './types'
+import { get, ENDPOINTS } from '~/store/api'
 
 export const setUserName = (userName: string) => (dispatch: Dispatch<ActionTypes>) => {
   dispatch({
     type: USER_ACTIONS.SET_USER_NAME,
     userName,
+  })
+
+  get(ENDPOINTS.SPELL_ACID_ARROW_ENDPOINT).then((resp) => {
+    console.log('resp', resp)
+    dispatch({
+      type: USER_ACTIONS.SET_USER_NAME,
+      userName,
+    })
   })
 }
